66cf2920425b029abdf4931bec37c441e06d0658
Oleh Kot <88795955+OlehKotynskyi@users.noreply.github.com>
2025-02-19 16:44:04 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -355,10 +355,10 @@
             }));
         }
     }), 0);
-    const leaf1 = __webpack_require__.p + "src/img/leaf1.png";
-    const leaf2 = __webpack_require__.p + "src/img/leaf2.png";
-    const leaf3 = __webpack_require__.p + "src/img/leaf3.png";
-    const leaf4 = __webpack_require__.p + "src/img/leaf4.png";
+    const leaf1 = __webpack_require__.p + "demo-sk/img/leaf4.webp";
+    const leaf2 = __webpack_require__.p + "demo-sk/img/leaf4.webp";
+    const leaf3 = __webpack_require__.p + "demo-sk/img/leaf4.webp";
+    const leaf4 = __webpack_require__.p + "demo-sk/img/leaf4.webp";
     if (window.innerWidth > 549.98) {
         const leafImages = [ leaf1, leaf2, leaf3, leaf4 ];
         const maxLeaves = 5;
